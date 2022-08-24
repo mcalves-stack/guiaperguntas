5fcf1030f4d5f1c5fcf979214b3d0cc53054fbcd
Mateus <57414186+mcalves-stack@users.noreply.github.com>
2022-08-24 16:11:26 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiaperguntas','root','Veritas@16',{
+const connection = new Sequelize('guiaperguntas','root','',{
   host: 'localhost',
   dialect: 'mysql'
 })
